1f6a12ddfa3d2c1f060631ebbc225c04e58df0cf
Guilherme Floriano N15 1A <minervino.guilherme@escola.pr.gov.br>
2025-06-06 08:39:16 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Alimentação',
+    'O que é uma alimentação balanceada?',
+    'Uma alimentação balanceada é aquela que inclui a quantidade certa de nutrientes para o corpo, como carboidratos, proteínas, gorduras, vitaminas e minerais.'
+)
+
+criaCartao(
+    'Alimentação',
+    'Qual a importância da água para o corpo?',
+    'A água é essencial para a sobrevivência, ajudando na digestão, no transporte de nutrientes e na regulação da temperatura corporal.'
+)
+
+criaCartao(
+    'Alimentação',
+    'O que são os macronutrientes?',
+    'Macronutrientes são nutrientes necessários em grandes quantidades, como carboidratos, proteínas e gorduras, que fornecem energia ao corpo.'
+)
+
+criaCartao(
+    'Alimentação',
+    'O que são micronutrientes?',
+    'Micronutrientes são nutrientes necessários em pequenas quantidades, como vitaminas e minerais, que são essenciais para o bom funcionamento do corpo.'
+)
